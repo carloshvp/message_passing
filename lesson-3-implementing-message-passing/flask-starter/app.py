82737c1c4fd8ee720518df8ce50131543d09aa19
Carlos Hernandez-Vaquero <carloshvp@gmail.com>
2021-10-30 17:27:33 +0200

    Experimenting with flask with a working REST API

--- lesson-3-implementing-message-passing/flask-starter/app.py
+++ lesson-3-implementing-message-passing/flask-starter/app.py
@@ -10,5 +10,15 @@ app = Flask(__name__)
 def health():
     return jsonify({'response': 'Hello World!'})
 
+@app.route('/api/items/<item_id>', methods=['GET', 'POST'])
+def items(item_id):
+    if request.method == 'GET':
+        return jsonify(retrieve_orders())
+    elif request.method == 'POST':
+        request_body = request.json
+        return jsonify(create_order(item_id))
+    else:
+        raise Exception('Unsupported HTTP request type.')
+
 if __name__ == '__main__':
     app.run()
